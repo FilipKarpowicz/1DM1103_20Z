c94dbb6c8c9a60f6e37d0ccb96505610af772222
filipkarpowicz <01159255@pw.edu.pl>
2020-10-22 17:08:21 +0200

    zadanie z suma podanych argumentow

---
+++ zad1/zadanie.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int a = 0;
+    if (argc == 1)
+    {
+        printf("nie podano ani jednej liczby\n");
+    }
+    else
+    {
+        for (int i = 0; i < argc; i++)
+        {
+            a = a + atof(argv[i]);
+        }
+        printf("suma tych liczb to %d\n", a);
+    }
+}
