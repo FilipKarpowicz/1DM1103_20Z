c8ce279f613693e65194c5463569bb7c451ae41f
filipkarpowicz <01159255@pw.edu.pl>
2021-01-07 23:34:03 +0100

    xd

---
+++ sem3/p2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char n[100];
+    //scrcpy(n, "Ala");
+    printf("ala\n", n);
+    return 0;
+}
